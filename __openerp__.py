950f869ac8824811a3517914ad25bd50a5976dba
dhecar <dhecar@gmail.com>
2016-02-26 14:36:54 +0100

    [IMP] clened

--- __openerp__.py
+++ __openerp__.py
@@ -29,10 +29,7 @@
     Need zebra python package https://pypi.python.org/pypi/zebra/
     """,
     "author": "David Hernández",
-    "depends": ['product', 'base_report_to_printer', 'web'],
-    #"js": ['static/src/js/labels.js'],
-    #"css": ['static/src/css/labels.css'],
-    #'qweb': ['static/src/xml/labels.xml'],
+    "depends": ['product', 'base_report_to_printer'],
     "update_xml": ['views/qz_print_config_view.xml', 'views/product_view.xml'],
     "data": [],
     "installable": True,
